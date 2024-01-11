dfa04412adbcbe86a66559537a568f00a021516a
Nepalimolly <121688585+Nepalimolly@users.noreply.github.com>
2024-01-11 16:33:53 -0600

    add origins

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://movie-mingle-fe.onrender.com"],
   })
 );
 app.use(cookieParser());
