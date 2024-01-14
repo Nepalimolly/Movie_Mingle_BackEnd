6644faaa49cf976b661eae16a1c8c686942d49bf
Nepalimolly <121688585+Nepalimolly@users.noreply.github.com>
2024-01-14 13:47:32 -0600

    added sql cloud connection

--- connect.js
+++ connect.js
@@ -1,9 +1,9 @@
 import mysql2 from "mysql2";
 
 export const db = mysql2.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Lukadon1996$",
-  database: "social",
+  host: "sql3.freemysqlhosting.net",
+  user: "sql3676952",
+  password: "pCvu21PPn2",
+  database: "sql3676952",
   port: 3306,
 });
