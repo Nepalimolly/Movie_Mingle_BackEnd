fd48dea4b852b8d4931322fff8ded5a1038b8014
Nepalimolly <121688585+Nepalimolly@users.noreply.github.com>
2024-01-18 14:16:42 -0600

    back to local database

--- connect.js
+++ connect.js
@@ -1,9 +1,8 @@
 import mysql2 from "mysql2";
 
 export const db = mysql2.createConnection({
-  host: "sql3.freemysqlhosting.net",
-  user: "sql3676952",
-  password: "pCvu21PPn2",
-  database: "sql3676952",
-  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "Lukadon1996$",
+  database: "social",
 });
--- index.js
+++ index.js
@@ -20,11 +20,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://movie-mingle-fe.onrender.com"],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-    optionsSuccessStatus: 204,
-    allowedHeaders: "Content-Type,Authorization",
+    origin: "http://localhost:3000",
   })
 );
 app.use(cookieParser());
